349a16030ab368581846ad6da2dccba6e96e7fee
Minji Kim <sml07113@gmail.com>
2025-11-21 15:37:51 +0900

    refactor: Service 어노테이션으로 Bean 자동 주입

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,5 +1,9 @@
 package com.back;
 
+import org.springframework.stereotype.Service;
+
+// 패키지 내 @Component, @Service, @Repository, @Controller 스캔 -> 자동으로 Bean으로 등록
+@Service
 public class PersonService {
     public long count() {
         return 3;
