e31e5de68005b3b6f65e3a2febf7771249f0c715
Minji Kim <sml07113@gmail.com>
2025-11-26 11:38:04 +0900

    feat: 10강 - Bean에 람다 함수 호출

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,7 +1,10 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
@@ -10,13 +13,40 @@ public class AppConfig {
         return 55;
     }
 
-//    @Bean
-//    public ApplicationRunner myApplicationRunner() {
-//        return new MyApplicationRunner();
-//    }
-//
-//    @Bean
-//    public ApplicationRunner myApplicationRunner2() {
-//        return new MyApplicationRunner();
-//    }
+    @Bean
+    @Order(2)
+    public ApplicationRunner myApplicationRunner() {
+        // 함수
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner 실행 중");
+            }
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public ApplicationRunner myApplicationRunner2() {
+        // 익명 함수(람다)
+        return args -> System.out.println("myApplicationRunner2 실행 중");
+    }
+
+    @Bean
+    @Order(3)
+    public ApplicationRunner myApplicationRunner3() {
+        // 익명 함수(람다)
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
+    }
 }
