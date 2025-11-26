d5cc2a394f2e0b0612febee2ab8141854035781a
Minji Kim <sml07113@gmail.com>
2025-11-26 11:15:25 +0900

    feat: 7강 - ApplicationRunner Bean 실습

---
+++ src/main/java/com/back/MyApplicationRunner.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("ApplicationArguments 실행 중");
+    }
+}
